396e4bb8bfe337355e959084ba3ef37608f274b3
akashmehrotra29 <akashmehrotra29@gmail.com>
2021-07-10 05:53:37 +0530

    fix: netlify reload, favicon, site name

--- src/common/api.config.js
+++ src/common/api.config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = `https://social-media-backend.akashmehrotra29.repl.co`;
+export const BASE_URL = `https://cape-share-backend.akashmehrotra29.repl.co`;
 // export const BASE_URL = `http://localhost:3001`;
